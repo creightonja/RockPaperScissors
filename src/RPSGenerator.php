07c584d534535add0380e57d40a2306930fb201e
creightonja <creightonja@gmail.com>
2015-08-12 20:40:48 -0700

    Update RPSGenerator.php

--- src/RPSGenerator.php
+++ src/RPSGenerator.php
@@ -5,8 +5,8 @@
             //Player one: rock = 0, paper = 1, scissors = 2
             //Player two: rock = 3, paper = 6, scissors = 9
             $rps_sum = $input1 + $input2;
-            $outcome_array = [3,3,3,0,1,2,2,0,1,1,2,0];  //Setting match winner index number into array
-            $result_array = ["Tie!", "Player 1 Wins!", "Player 2 Wins!"];  //Setting
+            $outcome_array = [3,3,3,0,1,2,2,0,1,1,2,0];  //Setting match winner index number into array at an index of the matching sums.
+            $result_array = ["Tie!", "Player 1 Wins!", "Player 2 Wins!"];  //Setting an array of possible results based on the value in outcome array
             return $result_array[$outcome_array[$rps_sum]];
         }
 
